74128d723789ced1dd92994e4d2d51a3bb4586af
Lucas Tan <sheng.x.tan@gmail.com>
2022-03-28 02:03:22 +0000

    created ways to determine number of offsprings, number of vampires from original, and whether a vampire is more senior than another

--- vampire.js
+++ vampire.js
@@ -1,31 +1,50 @@
+const { assert } = require('chai');
+
 class Vampire {
   constructor(name, yearConverted) {
     this.name = name;
     this.yearConverted = yearConverted;
-    this.offspring = [];
-    this.creator = null;
+    this.offspring = []; //subordinates
+    this.creator = null; //boss
   }
 
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    //add subordinate
+    //ada.addsubordinate(craig)
+    this.offspring.push(vampire);
+    vampire.creator = this
 
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    let numberOfOffspring = 0;
+    if (this.offspring.length > 0) {
+      numberOfOffspring = this.offspring.length;
+    }
+    return numberOfOffspring;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let vampiresFromOriginal = 0
+    let currentVampire = this;
+
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      vampiresFromOriginal++
+    }
+    return vampiresFromOriginal;
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    //numberofpeopletoCEO
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
